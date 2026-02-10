bf4ad5cacd6aef4b83a6a76b699b16fd3ee6a462
pabmarnav <pabmarnav6@alu.edu.gva.es>
2026-02-10 22:48:43 +0100

    Robust frontend URL logic

--- frontend/config/urls.prod.js
+++ frontend/config/urls.prod.js
@@ -1,2 +1,2 @@
 const backendEnv = "__BACKEND_URL__";
-const apiUrl = backendEnv.startsWith("__") ? "http://" + window.location.hostname + ":8080" : backendEnv;
+const apiUrl = (!backendEnv || backendEnv === "" || backendEnv.startsWith("__")) ? "http://" + window.location.hostname + ":8080" : backendEnv;
