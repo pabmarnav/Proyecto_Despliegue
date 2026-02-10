fe8990426e5645d2df9c790d352f440de62ea155
pabmarnav <pabmarnav6@alu.edu.gva.es>
2026-02-10 18:20:23 +0100

    Prepare for Render deployment

--- frontend/config/urls.prod.js
+++ frontend/config/urls.prod.js
@@ -1,1 +1,2 @@
-const apiUrl = "http://" + window.location.hostname + ":8080";
+const backendEnv = "__BACKEND_URL__";
+const apiUrl = backendEnv.startsWith("__") ? "http://" + window.location.hostname + ":8080" : backendEnv;
